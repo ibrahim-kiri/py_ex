6da244464be18eb19f55f70d43fcedf17b455794
ibrahim-kiri <ibrahimkiri@gmail.com>
2025-10-05 09:10:39 +0300

    Check if the first and last numbers of a list are the same

---
+++ exercise5.py
@@ -0,0 +1,18 @@
+# Checking if the first and last numbers of the list are the same
+def first_last_same(numberList):
+    print("Given list:", numberList)
+
+    first_num = numberList[0]
+    last_num = numberList[-1]
+
+    if first_num  == last_num:
+        return True
+    else:
+        return False
+
+numbers_x = [10, 20, 30, 40, 10]
+print("result is", first_last_same(numbers_x))
+
+numbers_y = [75, 65, 35, 75, 30]
+print("result is", first_last_same(numbers_y))
+
