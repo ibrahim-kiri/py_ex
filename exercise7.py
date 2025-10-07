92746cad7e166648449584497ffb96649abc18a8
ibrahim-kiri <ibrahimkiri@gmail.com>
2025-10-07 09:16:02 +0300

    Find the number of occurrences of a substring in a string

---
+++ exercise7.py
@@ -0,0 +1,24 @@
+# Find the number of occurrences of a substring in a string
+
+# Solution 1
+str_x = "Emma is good developer. Emma is a writer"
+
+# use count method of a str class
+cnt = str_x.count("Emma")
+print(cnt)
+
+# Solution 2
+def count_emma(statement):
+    print("Given String: ", statement)
+    count = 0
+    for i in range(len(statement) - 1):
+        count += statement[i: i + 4] == 'Emma'
+    return count
+
+count = count_emma("Emma is good developer. Emma is a write")
+print("Emma appeared ", count, "times")
+
+
+
+
+
