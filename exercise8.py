effbe0ab9c7b949c7856b70bcaaa14525a84edcb
ibrahim-kiri <ibrahimkiri@gmail.com>
2025-10-08 09:06:39 +0300

    Print each row containing the same number repeated

---
+++ exercise8.py
@@ -0,0 +1,9 @@
+# each row contains the same number repeated, and the number of repetitions increases with the row number.
+
+for num in range(10):
+
+    for i in range(num):
+        print(num, end=" ") # print number
+
+    # new line after each row to display pattern correctly
+    print("\n")
