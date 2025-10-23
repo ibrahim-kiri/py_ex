2b7d86620378d1052592df186f4f264c93f40d23
ibrahim-kiri <ibrahimkiri@gmail.com>
2025-10-23 10:31:52 +0300

    a simple countdown timer using a while loop

--- exercise23.py
+++ exercise23.py
@@ -1,1 +1,15 @@
 # simple countdown timer using a while loop.
+import time
+
+def countdown_timer(seconds):
+
+    while seconds > 0:
+        print(f"Time remaining: {seconds} seconds")
+        time.sleep(1) # Pause for 1 second
+        seconds -= 1
+
+    print("Time's up!")
+
+# Get the countdown duration from the user
+duration = 5
+countdown_timer(duration)
